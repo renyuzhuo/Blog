fc82e11d9e537032d63d4e25b71c8b524245afe1
RenYuZhuo <renyuzhuo@foxmail.com>
2019-07-28 01:05:21 +0800

    [bug]Fix Main HTTPS Bug

--- zzuhtzy/js/main.js
+++ zzuhtzy/js/main.js
@@ -155,7 +155,7 @@ function go(){
         MIT();
         return;
     }
-    if(url.includes("renyuzhuo.cn/index") || url.includes("renyuzhuo.cn/index.html") || url.length == 20 || url.length == 19 || url.includes("127.0.0.1:3000/404.html")){
+    if(url.includes("renyuzhuo.cn/index") || url.includes("renyuzhuo.cn/index.html") || url.length == 21 || url.length == 20 || url.includes("127.0.0.1:3000/404.html")){
         console.log("index");
         index();
         return;
